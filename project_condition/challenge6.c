8e42b4d93170ab1f69e5024291bddc4ccac643b1
holly ufo <54860557+hollyufo@users.noreply.github.com>
2021-11-14 13:02:07 +0100

    CHAALENGZ

--- project_condition/challenge6.c
+++ project_condition/challenge6.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 #include<math.h>
 int main(){
-	char pal[]="50";
+	char pal[20]="50";
 	printf("doner un palindrome pal ");
 	scanf("%s",pal);
-	if (pal[0]==pal[1]&&pal[3]==pal[4])
+	if (pal[0] == pal[4] && pal[3] == pal[1])
 	printf("palindrome");
 	else
 	printf("not");
